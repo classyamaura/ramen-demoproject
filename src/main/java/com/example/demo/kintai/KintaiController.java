33999c7b8b6ec548c8355168a96a54913f6fa902
Your Name <you@example.com>
2025-06-25 14:47:52 +0900

    kinntai

--- src/main/java/com/example/demo/kintai/KintaiController.java
+++ src/main/java/com/example/demo/kintai/KintaiController.java
@@ -1,5 +1,24 @@
 package com.example.demo.kintai;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class KintaiController {
+
+	//トップページの表示
+	@RequestMapping("/kintaitop")
+	public String kintaitop(Model model) {
+
+		return "kintai/Kintai_top";
+	}
+	//管理者ログイン画面
+	@RequestMapping("/login")
+	public String login(Model model) {
+
+		return "kintai/Kintai_login";
+	}
+
 
 }
