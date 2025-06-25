f2b1016b89da025a26820a83f5bc26ef0b70a9b8
yamagata <you@example.com>
2025-06-25 11:22:19 +0900

    topページをブラウザに表示しました

--- src/main/java/com/example/demo/shift/ShiftController.java
+++ src/main/java/com/example/demo/shift/ShiftController.java
@@ -1,5 +1,17 @@
 package com.example.demo.shift;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ShiftController {
 
+
+
+@RequestMapping("/top")
+public String top(Model model) {
+	return "shift/shift-index";
+	
+}
 }
