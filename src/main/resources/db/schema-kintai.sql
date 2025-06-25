445314ca182c7f29f78c88e154a0eddbb681b816
nishimura <you@example.com>
2025-06-25 13:30:15 +0900

    kintaiのデータベース関係のところです。

--- src/main/resources/db/schema-kintai.sql
+++ src/main/resources/db/schema-kintai.sql
@@ -1,3 +1,10 @@
 -- 勤怠関連テーブルのスキーマ定義用ファイル
 SELECt 1;
 -- TODO:
+
+CREATE TABLE kintai (
+    name VARCHAR(100) NOT NULL,            -- 従業員の名前
+    start_time TIMESTAMP NOT NULL,         -- 出勤時間
+    end_time TIMESTAMP NOT NULL,           -- 退勤時間
+    work_time INTERVAL DAY TO SECOND NOT NULL  -- 勤務時間（INTERVAL型）
+);
